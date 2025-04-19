5d210a62ae850959cf0ced06ca25c979edb1e39a
Ubuntu <azureuser@WorkStation-Gamma.sz4jluy1mnke5hqz5ea5piznuc.ux.internal.cloudapp.net>
2025-04-19 16:58:03 +0000

    modification: jenkins, pom and index

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
